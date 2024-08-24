fb0e8035d1df2a9f0a8617a4237a9a83e21d36e9
EZZYE <danielemmanuel48258@outlook.com>
2024-08-24 20:51:42 +0100

    navigation.py

--- navigation.py
+++ navigation.py
@@ -1,1 +1,19 @@
-print("hello,Emma")
+#let me store my favourite number
+favourite_number = 80,
+print(f"my favourite number is {favourite_number}") 
+#how about a word (string)?
+greeting = "hello, python"
+print("greeting")
+#and a boolean
+is_python_fun = True
+print(f"is python fun? {is_python_fun}")
+#weather measurement
+temperature = 40
+if temperature > 30:
+    print("it's hot outside! wear shorts.")
+else:
+    print("barrrr! it's cold. bundle up!")
+if temperature >= 30:
+    print("normal weahter")
+else:
+    print("adnormal weather")
